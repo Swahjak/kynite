5e3372e647419c77cc583c710d52b401c47e66f0
Peep van Puijenbroek <peep@toppy.nl>
2025-12-21 18:04:34 +0100

    feat: add calendar demo page

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,9 @@
+import { Calendar } from "@/components/calendar/calendar";
+
+export default function CalendarPage() {
+  return (
+    <main className="bg-background min-h-screen">
+      <Calendar />
+    </main>
+  );
+}
