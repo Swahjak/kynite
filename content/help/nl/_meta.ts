397846a95c48f552dd0ecbfcd09c75436a6ceb83
Peep van Puijenbroek <peep@toppy.nl>
2025-12-27 12:32:19 +0100

    feat: add initial help content structure

---
+++ content/help/nl/_meta.ts
@@ -0,0 +1,8 @@
+export default {
+  index: "Welkom",
+  "getting-started": "Aan de slag",
+  calendar: "Kalender",
+  chores: "Klusjes & Sterren",
+  rewards: "Beloningswinkel",
+  "wall-hub": "Wandscherm",
+};
