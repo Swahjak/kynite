960421f1b4663b9110b65615231f46e3664ce748
Peep van Puijenbroek <peep@toppy.nl>
2025-12-28 20:24:22 +0100

    feat: add Media collection for Payload CMS

--- payload.config.ts
+++ payload.config.ts
@@ -3,13 +3,15 @@ import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { buildConfig } from "payload";
 import { Users } from "./src/payload/collections/Users";
+import { Media } from "./src/payload/collections/Media";
+import { Pages } from "./src/payload/collections/Pages";
 
 export default buildConfig({
   admin: {
     user: Users.slug,
   },
   editor: lexicalEditor(),
-  collections: [Users],
+  collections: [Users, Media, Pages],
   secret: process.env.PAYLOAD_SECRET || "",
   db: postgresAdapter({
     pool: {
